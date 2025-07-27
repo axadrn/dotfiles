d07958cc1f4c518a348462e21a37707e9571a42a
axzilla <hello@axeladrian.com>
2025-07-27 23:12:13 +0700

    simplify neovim config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      -- vim.cmd("colorscheme gruvbox")
-    end,
-    opts = {}
-  },
-  {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
-      -- vim.cmd.colorscheme('gruvbox-material')
-    end
-  },
-  {
     "rebelot/kanagawa.nvim",
     config = function()
       require("kanagawa").setup({
