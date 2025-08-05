3c9cb0ad01e4696d3be3c2d9ee67a05515b5811d
axzilla <hello@axeladrian.com>
2025-08-05 13:17:51 +0700

    add hammerspoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,17 @@
+local apps = {
+  T = "Ghostty",
+  C = "Google Chrome",
+  S = "Safari",
+}
+
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+
+for key, appName in pairs(apps) do
+  hs.hotkey.bind(hyper, key, function()
+    hs.application.launchOrFocus(appName)
+  end)
+end
+
+hs.hotkey.bind(hyper, "", function()
+  hs.urlevent.openURL("https://twitter.com/intent/tweet")
+end)
