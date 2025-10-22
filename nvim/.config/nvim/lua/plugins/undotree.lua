d75045daa312eb5ceb30481d3918df48d5434de5
axzilla <hello@axeladrian.com>
2025-10-22 11:37:07 +0400

    cleanup

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"mbbill/undotree",
-	config = function()
-		vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle, { desc = "Toogle Undotree" })
-		vim.g.undotree_SetFocusWhenToggle = 1
-	end,
-}
